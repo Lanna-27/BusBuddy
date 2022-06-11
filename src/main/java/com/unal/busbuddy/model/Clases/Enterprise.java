fd2df70304926b455533694091d3e6da0973c05f
Lanna-27 <lalacast2000@gmail.com>
2022-06-11 14:28:12 -0500

    actualizar conexion bd

--- src/main/java/com/unal/busbuddy/model/Clases/Enterprise.java
+++ src/main/java/com/unal/busbuddy/model/Clases/Enterprise.java
@@ -1,5 +1,20 @@
 package com.unal.busbuddy.model.Clases;
 
-public class Enterprise {
+public class Enterprise extends User {
+    private long NIT;
+
+    public Enterprise(int idUser, String nickname, String firstName, String lastName, String email, String password, long telNumber, String birthDate, long NIT) {
+        super(idUser,nickname, firstName, lastName, email, password, telNumber, birthDate);
+        this.NIT = NIT;
+        
+    }
+
+    @Override
+    public String toString(){
+        return "Usuario: " + getNickname() + "\n"+
+               "Nombre completo: " + getFirstName() +" "+ getLastName()+"\n"+
+               "Numero de telefono: " + String.valueOf(getTelNumber()) +"\n"+
+               "Fecha de nacimiento: " + getBirthDate();
+    }
     
 }
